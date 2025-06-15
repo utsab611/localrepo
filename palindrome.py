c1dc409e682efe8c61086b8f579793d2b1286743
utsab611 <utsabmukherjee611@gmail.com>
2025-06-15 22:44:50 +0530

    Code updgraded

--- palindrome.py
+++ palindrome.py
@@ -1,17 +1,18 @@
-class palindrome:
-    def __init__(self,s):
-        self.word=s
-    def check(self):
-        if self.word==self.word[::-1]:
-            print("The word is palindrome")
-        else:
-            print("The word is not palindrome")
+class Palindrome:
+    def __init__(self, word: str):
+        self.word = word
 
-if __name__=="__main__":
-    # changed the value of the word
-    word="Utsabastu"
-    palin_check=palindrome(word)
-    palin_check.check()
+    def is_palindrome(self) -> bool:
+        normalized = self.word.lower()
+        return normalized == normalized[::-1]
+
+    def __str__(self):
+        return f'"{self.word}" is a palindrome.' if self.is_palindrome() else f'"{self.word}" is not a palindrome.'
+
+if __name__ == "__main__":
+    word = "Utsabastu"
+    palin_check = Palindrome(word)
+    print(palin_check)
 
 
 
